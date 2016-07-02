895b7be25e1a6946c5b7b16fb0c01205de043581
Francesco 'makevoid' Canessa <makevoid@gmail.com>
2016-07-02 11:47:18 +0100

    update misc scripts

--- misc/send_ethers.js
+++ misc/send_ethers.js
@@ -1,3 +1,9 @@
-eth.sendTransaction({from: "0x7b3a44a832e5f2a5c0fd55a2982f050b738aed18", to: "0x622718cce4c6b75923ca21e5b1293e9fd4dae25a", value: web3.toWei(1, "ether")})
+Web3 = require('web3'); web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
-Number(eth.getBalance("0x622718cce4c6b75923ca21e5b1293e9fd4dae25a"))
+addrFrom = "0x7b3a44a832e5f2a5c0fd55a2982f050b738aed18"
+addrTo   = "0x622718cce4c6b75923ca21e5b1293e9fd4dae25a"
+
+eth.sendTransaction({from: addrFrom, to: addrTo, value: web3.toWei(1, "ether")})
+
+// wait the tx to be confirmed...
+Number(eth.getBalance(addrTo))
